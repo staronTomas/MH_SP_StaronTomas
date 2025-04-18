3d6c64da02c90161cbdbf5840fa8d42055cf6665
Tomáš Staroň <tomas.staronn@gmail.com>
2025-04-18 18:56:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MH_SP_StaronTomas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
